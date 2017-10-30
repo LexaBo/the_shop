44f9a0f5d7c60bbf4e934ca86ec2c26e87273e27
LexaBo <32167787+LexaBo@users.noreply.github.com>
2017-10-30 14:18:08 +0300

    Add switch

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,11 @@
-// Your code here...
+document.addEventListener('click',function (event) {
+    if(event.target.classList.contains('color__button_white')){
+        document.getElementById('big').src = 'img/tshirt_white.jpg';
+    }
+    if(event.target.classList.contains('color__button_yellow')){
+        document.getElementById('big').src = 'img/tshirt_yellow.jpg';
+    }
+    if(event.target.classList.contains('color__button_green')){
+        document.getElementById('big').src = 'img/tshirt_green.jpg';
+    }
+});
